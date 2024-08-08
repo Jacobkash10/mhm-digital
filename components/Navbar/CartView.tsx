30cdf8790a662579c313962fcfec5eecebb18d98
Jacobkash10 <86664678+Jacobkash10@users.noreply.github.com>
2024-08-08 23:43:29 +0100

    cart update

--- components/Navbar/CartView.tsx
+++ components/Navbar/CartView.tsx
@@ -41,7 +41,7 @@ const CartView = ({openCart, open}: {openCart: () => void, open: boolean}) => {
       className='w-full z-50 fixed top-0 left-0 inset-0 flex items-center'>
             <div className='bg-[#000000b4] w-[62%] h-full' onClick={openCart}>
             </div>
-            <div className='w-[80%] md:w-[40%] bg-white shadow h-full'>
+            <div className='w-[100%] md:w-[40%] bg-white shadow h-full'>
                   <div className='border-b-2 py-6'>
                         <div className='flex justify-between items-center px-5'>
                               <h2 className='text-2xl font-bold'>Your Cart</h2>
@@ -72,7 +72,7 @@ const CartView = ({openCart, open}: {openCart: () => void, open: boolean}) => {
                                     <div className='px-6 py-1'>
                                           {carts?.map((item) => (
                                                 <div key={item.id} className='mt-5 border-b py-4'>
-                                                      <div className='flex items-start justify-between'>
+                                                      <div className='row sm:flex items-start justify-between'>
                                                             <div className='flex items-start gap-5'>
                                                                   <div className='w-[20%]'>
                                                                         <Image src={image1} alt='image1' priority width={0} height={0} 
@@ -105,7 +105,7 @@ const CartView = ({openCart, open}: {openCart: () => void, open: boolean}) => {
                                                       </div>
                                                 </div>
                                           ))}
-                                          <div className='flex items-center justify-between mt-5'>
+                                          <div className='row sm:flex items-center justify-between mt-5'>
                                                 <h5 className='text-slate-500 font-bold'>Subtotal</h5>
                                                 <p className='font-semibold'>
                                                       $ {carts?.map((item) => item.packageDuration)?.reduce((total, item) => 
@@ -119,7 +119,7 @@ const CartView = ({openCart, open}: {openCart: () => void, open: boolean}) => {
                                                       className='flex items-center justify-center gap-2 bg-red-500 text-white 
                                                       rounded-full px-10 py-6 
                                                       shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] w-full '>
-                                                            <h5 className='font-semibold text-base'>
+                                                            <h5 className='font-semibold text-xs sm:text-base'>
                                                                   Continue To Checkout
                                                             </h5>
                                                       </motion.button>
